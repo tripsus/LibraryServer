ffa76ed076c383cca70e190b93b25a43182f2235
Sushil Tripathi <sushiltripathi72@gmail.com>
2021-01-03 15:00:24 +0545

    Post request working

--- app.py
+++ app.py
@@ -7,15 +7,37 @@ app = Flask(__name__)
 file_handle = None
 books_list = []
 
+def add_book(title, author, genre):
+    global books_list
+    book = {
+        'title': None,
+        'author': None,
+        'genre': None
+    }
+    book['title'] = title
+    book['author'] = author
+    book['genre'] = genre
+    books_list.append(book)
 
-@app.route('/books')
+@app.route('/books', methods=['POST','GET'])
 def books():
+    global books_list
     response = {
         "message": "There are no books in libarary. Please add books",
         "books": []
     }
     # import pdb; pdb.set_trace()
-    # print(request.environ['REQUEST_METHOD'])
+    print(request.form)
+    if (request.environ['REQUEST_METHOD'] == 'POST'):
+        add_book(request.form['title'],request.form['author'],request.form['genre'])
+        response['books'] = books_list
+        response['message'] = 'We have found some books in your library'
+
+    else:
+        if (len(books_list) > 0):
+            response['books'] = books_list
+            response['message'] = 'We have found some books in your library'
+
     return response
 
 @app.route('/')
@@ -25,4 +47,4 @@ def hello():
     return "Welcome to our library! How can we help you today"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=1)
